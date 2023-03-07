bf67f0536cfad6b72591ad48762fe219f2091ebb
StefanoM98 <smartino030@gmail.com>
2023-03-07 13:57:18 +0100

    Add: prompt surname user

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@
 // CHIEDERE IL NOME ALL'UTENTE
 const nameuser = prompt ("Qual è il tuo nome?")
 console.log(nameuser);
+
+// CHIEDIMO IL COGNOME AL'UTENTE
+const surnameuser = prompt ("Qual è il tuo cognome?")
+console.log(surnameuser);
